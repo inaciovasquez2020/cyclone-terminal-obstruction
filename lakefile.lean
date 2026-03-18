c948d5117d252c570625bdb0f537727a61d07fdc
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-18 15:16:05 -0400

    Manually add mathlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -1,13 +1,10 @@
 import Lake
 open Lake DSL
 
-package cyclone where
+package «cyclone-terminal-obstruction» where
 
-require std from git
-  "https://github.com/leanprover/std4.git" @ "v4.27.0"
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_lib Cyclone
-
-lean_exe cyclone where
-  root := `Cyclone.Slim.Main
