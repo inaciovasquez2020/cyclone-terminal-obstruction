01cf425536ef40466a5cbf2615d665f4c0e73f9f
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-26 00:10:47 -0500

    Add FinGraph structure for explicit edge/vertex incidence representation

--- lean/Oblivion/BoundaryMap.lean
+++ lean/Oblivion/BoundaryMap.lean
@@ -19,3 +19,15 @@ noncomputable def boundaryMap : C1 Γ →ₗ[ℚ] C0 Γ :=
 
 noncomputable def beta1 : ℕ :=
   finrank ℚ (LinearMap.ker (boundaryMap Γ))
+
+class FinGraph extends Graph where
+  E : Type u
+  src : E → V
+  dst : E → V
+  finite_V : Fintype V
+  finite_E : Fintype E
+  decEq_V : DecidableEq V
+  decEq_E : DecidableEq E
+  adj_iff :
+    ∀ u v : V, Adj u v ↔ ∃ e : E, (src e = u ∧ dst e = v) ∨ (src e = v ∧ dst e = u)
+
