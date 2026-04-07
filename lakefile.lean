19dee8fd595c3c12ed6b8541fcb28b20d5fe444e
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-07 20:14:35 -0300

    Declare mathlib dependency in Lake configuration

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,11 @@
 import Lake
 open Lake DSL
 
-package «cyclone-terminal-obstruction» {
+package «cyclone-terminal-obstruction» where
   -- optional metadata
-}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_lib CycloneTerminalObstruction
