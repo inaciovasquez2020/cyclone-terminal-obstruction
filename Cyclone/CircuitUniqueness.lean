238b51e5a01137fbf5c8162e111b3e2c88957c69
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-07 21:35:52 -0300

    Fix CircuitUniqueness declaration headers and determinant reference

--- Cyclone/CircuitUniqueness.lean
+++ Cyclone/CircuitUniqueness.lean
@@ -1,4 +1,5 @@
 import Mathlib.Data.Matrix.Basic
+import Mathlib.LinearAlgebra.Matrix.Determinant
 import Mathlib.Data.ZMod.Basic
 import Mathlib.Data.Finset.Basic
 
@@ -6,23 +7,25 @@ open scoped BigOperators
 
 def GLEquiv {n r : Nat} (A B : Matrix (Fin r) (Fin n) (ZMod 2)) : Prop :=
   ∃ (P : Matrix (Fin r) (Fin r) (ZMod 2)) (σ : Equiv.Perm (Fin n)),
-    IsUnit P.det ∧ B = P * (A.submatrix id σ)
+    IsUnit (Matrix.det P) ∧ B = P * (A.submatrix id σ)
 
-def CircuitSupport {n : Nat} := Finset (Fin n)
+def CircuitSupport (n : Nat) := Finset (Fin n)
 
-def PermEquivSupport {n : Nat} (S T : CircuitSupport) : Prop :=
+def PermEquivSupport {n : Nat} (S T : CircuitSupport n) : Prop :=
   ∃ σ : Equiv.Perm (Fin n), T = S.map σ.toEmbedding
 
-def SizeKPlusOne {n k : Nat} (S : CircuitSupport) : Prop := S.card = k + 1
+def SizeKPlusOne {n k : Nat} (S : CircuitSupport n) : Prop := S.card = k + 1
 
-def BinaryMatroidCircuit {n r : Nat} (A : Matrix (Fin r) (Fin n) (ZMod 2)) (S : CircuitSupport) : Prop := True
+def BinaryMatroidCircuit {n r : Nat}
+    (A : Matrix (Fin r) (Fin n) (ZMod 2)) (S : CircuitSupport n) : Prop := True
 
 theorem circuit_uniqueness_size_k_plus_one
     {n r k : Nat}
     (A : Matrix (Fin r) (Fin n) (ZMod 2))
+    (S T : CircuitSupport n)
     (h1 : BinaryMatroidCircuit A S)
     (h2 : BinaryMatroidCircuit A T)
-    (hS : SizeKPlusOne (n := n) (k := k) S)
-    (hT : SizeKPlusOne (n := n) (k := k) T) :
+    (hS : SizeKPlusOne (k := k) S)
+    (hT : SizeKPlusOne (k := k) T) :
     PermEquivSupport S T := by
   sorry
