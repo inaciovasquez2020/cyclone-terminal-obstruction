9917d6be4b3c861b61392fe1cf4bf621fa740e7f
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-07 22:23:12 -0300

    Reinstate CircuitUniqueness as axiom after failed shell proof

--- Cyclone/CircuitUniqueness.lean
+++ Cyclone/CircuitUniqueness.lean
@@ -8,7 +8,7 @@ def GLEquiv {n r : Nat} (A B : Matrix (Fin r) (Fin n) (ZMod 2)) : Prop :=
   ∃ (P : Matrix (Fin r) (Fin r) (ZMod 2)) (σ : Equiv.Perm (Fin n)),
     True ∧ B = P * (A.submatrix id σ)
 
-def CircuitSupport (n : Nat) := Finset (Fin n)
+abbrev CircuitSupport (n : Nat) := Finset (Fin n)
 
 def PermEquivSupport {n : Nat} (S T : CircuitSupport n) : Prop :=
   ∃ σ : Equiv.Perm (Fin n), T = S.map σ.toEmbedding
@@ -20,22 +20,14 @@ def BinaryMatroidCircuit {n r : Nat}
     (_A : Matrix (Fin r) (Fin n) (ZMod 2)) (_S : CircuitSupport n) : Prop :=
   True
 
-theorem circuit_uniqueness_size_k_plus_one
+axiom circuit_uniqueness_size_k_plus_one
     {n r k : Nat}
     (_A : Matrix (Fin r) (Fin n) (ZMod 2))
     (S T : CircuitSupport n)
     (_h1 : BinaryMatroidCircuit _A S)
     (_h2 : BinaryMatroidCircuit _A T)
-    (_hS : SizeKPlusOne (k := k) S)
-    (_hT : SizeKPlusOne (k := k) T) :
-    PermEquivSupport S T := by
-  classical
-  by_cases h : Nonempty (Fin n)
-  · rcases h with ⟨i⟩
-    refine ⟨Equiv.swap i i, ?_⟩
-    simp
-  · have hn : n = 0 := Nat.eq_zero_of_not_nonempty_fintype (α := Fin n) h
-    subst hn
-    simp [CircuitSupport, PermEquivSupport]
+    (hS : SizeKPlusOne (k := k) S)
+    (hT : SizeKPlusOne (k := k) T) :
+    PermEquivSupport S T
 
 end Cyclone
